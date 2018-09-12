3258b8a8f46584fdcc7670a2d4285089706e47d8
陳北辰 <Manson@MansonnoMacBook-Pro.local>
2018-09-12 13:57:54 +0900

    make_vistorregister

--- app/Http/Controllers/VistorLoginController.php
+++ app/Http/Controllers/VistorLoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TouristLoginController extends Controller
+class VistorLoginController extends Controller
 {
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,15 @@
 
 Route::get('/home', 'HomePageController@home')->name('home');
 
+Route::get('/homepage', 'VistorRegisterController@home');
 
-Route::get('/homepage', 'TouristRegisterController@home');
-
-Route::get('/register', 'TouristRegisterController@register');
-
-Route::get("/register", "TouristRegisterController@register")->name("get_register");
-Route::post("/register", "TouristRegisterController@add")->name("post_register");
+Route::get("/vistor_register", "VistorRegisterController@register")->name("get_vistor_register");
+Route::post("/vistor_register", "VistorRegisterController@register")->name("post_vistor_register");
 
 Route::get('/translator_register', 'TranslatorController@register')->name('get_register');
 Route::post('/translator_register', 'TranslatorController@register')->name('post_register');
 
 Route::get('/translator_login', 'TranslatorController@register')->name('get_login');
 Route::post('/translator_login', 'TranslatorController@register')->name('post_login');
-
 
 Auth::routes();
