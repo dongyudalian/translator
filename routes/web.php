194d7e0e9503986427e9077697b09076748967e7
haihaizhi <jianglei881015@163.com>
2018-09-14 12:31:13 +0900

    make migration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Route::get('/homepage', 'HomePageController@home')->name('visitor_homepage');
 
-Route::get("/search", "SearchController@index")->name('get_search');
+Route::get("/search", "SearchController@index")->name('get_search')->middleware("auth:visitor");
 Route::get("/translator_info/{id}", "TranslatorInfosController@index")->name('get_translator_info');
 
 Route::get("/visitor_register", "VisitorRegisterController@register")->name("get_visitor_register");
@@ -19,5 +19,7 @@ Route::get('/translator_login', 'TranslatorController@login')->name('get_login')
 Route::post('/translator_login', 'TranslatorController@login')->name('post_login');
 Route::get('/logout', 'TranslatorController@logout')->name('logout');
 Route::get('/choose_time', 'ChooseController@time')->name('choose');
+
+Route::get('/reservation','ReservationsController@index')->name('get_reservation');
 
 Auth::routes();
