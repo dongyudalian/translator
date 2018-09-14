866359418b31f401374b35fead77c73d9a539ecd
LinXi <linxi@LinXis-MacBook-Air.local>
2018-09-14 14:22:45 +0900

    change route

--- resources/views/translator/translator_register.blade.php
+++ resources/views/translator/translator_register.blade.php
@@ -4,8 +4,6 @@
         <meta charset="utf-8">
         <title>新規登録</title>
     </head>
-    <img src="japan1.png" alt="" width="auto" height="auto">
-
     <body>
         @if(count($errors) > 0)
             <ul>
@@ -67,21 +65,20 @@
                         <td>
                             <select class="select_salary" id="translator_salary" name="translator_salary">
                                 <option value="">時給を選択</option>
-                                <option value="1">1000円/時</option>
-                                <option value="2">1500円/時</option>
-                                <option value="3">2000円/時</option>
-                                <option value="4">2500円/時</option>
+                                @foreach($aas as $aa)
+                                    <option value="{{$aa->id}}">{{$aa->value}}</option>
+                                @endforeach
                             </select>
                         </td>
                         <td>
-                            <input type="checkbox" name="translator_specialities[]" value="1">生活
-                            <input type="checkbox" name="translator_specialities[]" value="2">商務
+                            @foreach($bbs as $bb)
+                                <input type="checkbox" name="translator_specialities[]" value="{{$bb->id}}">{{$bb->value}}
+                            @endforeach
                         </td>
                         <td>
-                            <input type="checkbox" name="translator_statures[]" value="1">運動
-                            <input type="checkbox" name="translator_statures[]" value="2">淑やか
-                            <input type="checkbox" name="translator_statures[]" value="3">可愛い
-                            <input type="checkbox" name="translator_statures[]" value="4">成熟
+                            @foreach($ccs as $cc)
+                                <input type="checkbox" name="translator_statures[]" value="{{$cc->id}}">{{$cc->value}}
+                            @endforeach
                         </td>
                         <td>
                             <select class="select_license" id="translator_license" name="translator_license">
@@ -102,75 +99,14 @@
                 <h3>自己紹介</h3>
                 <textarea name="translator_self" rows="14" cols="140"></textarea>
                 <h3>行ける場所</h3>
-                <input type="checkbox" name="translator_ikus[]" value="1">東京都
-                <input type="checkbox" name="translator_ikus[]" value="2">大阪
-                <input type="checkbox" name="translator_ikus[]" value="3">北海道
-                <input type="checkbox" name="translator_ikus[]" value="4">冲绳
-                <input type="checkbox" name="translator_ikus[]" value="5">横滨
-                <input type="checkbox" name="translator_ikus[]" value="6">京都
+                @foreach($dds as $dd)
+                    <input type="checkbox" name="translator_ikus[]" value="{{$dd->id}}">{{$dd->value}}
+                @endforeach
                 <h3>空い時間選択</h3>
                 <div class="row" style="margin-top:50px">
-                  <div class="col-6">
-                    本月空闲时间:
-                    <table border='1' align='center'>10月</br>
-                      <tr>
-                        <th>星期日</th>
-                        <th>星期一</th>
-                        <th>星期二</th>
-                        <th>星期三</th>
-                        <th>星期四</th>
-                        <th>星期五</th>
-                        <th>星期六</th>
-                      </tr>
-                      <tr>
-                            <td width='80px'></td>
-                            <td width='80px'><input type='checkbox' name='translator_times[]'value='2018-10-01'>1</td>
-                            <td width='80px'><input type='checkbox' name='translator_times[]'value='2018-10-02'>2</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>3</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>4</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>5</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>6</td>
-                        </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>7</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>8</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>9</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>10</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>11</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>12</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>13</td>
-                        </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>14</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>15</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>16</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>17</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>18</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>19</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>20</td>
-                        </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>21</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>22</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>23</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>24</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>25</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>26</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>27</td>
-                        </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>28</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>29</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>30</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>31</td>
-                        </tr>
-                    </table>
-                  </div>
-
-
-                  <div class="col-6">
-                    次月空闲时间:
-                    <table border='1' align='center'>11月</br>
+                    <div class="col-6">
+                        <p>{{$mon}}月</p>
+                    <table border='1' align='center'></br>
                         <tr>
                             <th>星期日</th>
                             <th>星期一</th>
@@ -180,52 +116,98 @@
                             <th>星期五</th>
                             <th>星期六</th>
                         </tr>
+                        @for($j=1;$j<=count($arr);$j++)
+                            @php
+                            $n++;
+                            @endphp
+                            @if($n==1)
+                                <tr>
+                            @endif
+                            @if($mday <= $arr[$j-1])
+                                <td width='80px'><input type='checkbox' name='translator_times[]' value='{{$year}}-{{$mon}}-{{$arr[$j-1]}}'>{{$arr[$j-1]}}</td>
+                            @else
+                                <td width='80px'>&nbsp&nbsp&nbsp&nbsp{{$arr[$j-1]}}</td>
+                            @endif
+                            @if($n==7)
+                                </tr>
+                                @php
+                                $n=0
+                                @endphp
+                            @endif
+                        @endfor
+                        @if($n!=7)
+                            </tr>
+                        @endif
+                    </table></br></br>
+                    <p>{{$mon1}}月</p>
+                    <table border='1' align='center'>
                         <tr>
-                            <td width='80px'></td>
-                            <td width='80px'></td>
-                            <td width='80px'></td>
-                            <td width='80px'></td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>1</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>2</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>3</td>
+                            <th>星期日</th>
+                            <th>星期一</th>
+                            <th>星期二</th>
+                            <th>星期三</th>
+                            <th>星期四</th>
+                            <th>星期五</th>
+                            <th>星期六</th>
                         </tr>
+                        @for($j=1;$j<=count($arr1);$j++)
+                            @php
+                            $n1++;
+                            @endphp
+                            @if($n1==1)
+                                <tr>
+                            @endif
+                            @if($mday1 = $arr1[$j-1])
+                                <td width='80px'><input type='checkbox' name='translator_times[]' value='{{$year1}}-{{$mon1}}-{{$arr1[$j-1]}}'>{{$arr1[$j-1]}}</td>
+                            @else
+                                <td width='80px'>&nbsp&nbsp&nbsp&nbsp{{$arr1[$j-1]}}</td>
+                            @endif
+                            @if($n1==7)
+                                </tr>
+                                @php
+                                $n1=0
+                                @endphp
+                            @endif
+                        @endfor
+                        @if($n1!=7)
+                            </tr>
+                        @endif
+                    </table></br></br>
+                    <p>{{$mon2}}月</p>
+                    <table border='1' align='center'>
                         <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>4</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>5</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>6</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>7</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>8</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>9</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>10</td>
+                            <th>星期日</th>
+                            <th>星期一</th>
+                            <th>星期二</th>
+                            <th>星期三</th>
+                            <th>星期四</th>
+                            <th>星期五</th>
+                            <th>星期六</th>
                         </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>11</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>12</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>13</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>14</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>15</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>16</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>17</td>
-                        </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>18</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>19</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>20</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>21</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>22</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>23</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>24</td>
-                        </tr>
-                        <tr>
-                            <td width='80px'><input type='checkbox' name='translator_time'>25</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>26</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>27</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>28</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>29</td>
-                            <td width='80px'><input type='checkbox' name='translator_time'>30</td>
-                        </tr>
-                    </table>
-                  </div>
+                        @for($j=1;$j<=count($arr2);$j++)
+                            @php
+                            $n2++;
+                            @endphp
+                            @if($n2==1)
+                                <tr>
+                            @endif
+                            @if($mday2 = $arr2[$j-1])
+                                <td width='80px'><input type='checkbox' name='translator_times[]'value='{{$year2}}-{{$mon2}}-{{$arr2[$j-1]}}'>{{$arr2[$j-1]}}</td>
+                            @else
+                                <td width='80px'>&nbsp&nbsp&nbsp&nbsp{{$arr2[$j-1]}}</td>
+                            @endif
+                            @if($n2==7)
+                                </tr>
+                                @php
+                                $n2=0
+                                @endphp
+                            @endif
+                        @endfor
+                        @if($n2!=7)
+                            </tr>
+                        @endif
+                    </table></br></br>
+                    </div>
                 </div>
                 <input type="submit" value="送る">
             </form>
